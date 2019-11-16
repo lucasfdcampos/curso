79ecab8af32243e53f1bba576508437a468e6556
lucasfdcampos <_83_wm_tz_>
2019-11-16 11:00:58 -0300

    Repositories.

---
+++ src/main/java/br/com/curso/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package br.com.curso.repository;
+
+import br.com.curso.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+}
